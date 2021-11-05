9c26b6c45b49af87805d811229772fe530ad7dc9
wiem.fourati <wiem.fourati@sastec-group.com>
2021-11-05 16:17:10 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/wiemsastec/gqlgen-todos
+
+go 1.13
+
+require (
+	github.com/99designs/gqlgen v0.14.0
+	github.com/gin-gonic/gin v1.7.4
+	github.com/vektah/gqlparser/v2 v2.2.0
+)
---
+++ server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/handler"
+	"github.com/gin-gonic/gin"
+	"github.com/wiemsastec/gqlgen-todos/graph"
+	"github.com/wiemsastec/gqlgen-todos/graph/generated"
+)
+
+const defaultPort = "8080"
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := newFunction()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func newFunction() http.HandlerFunc {
+	h := handler.GraphQL(NewExecutableSchema(Config{Resolvers: &Resolver{}}))
+	return h
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := handler.Playground("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+func main() {
+	// Setting up Gin
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{
+		Resolvers:  &graph.Resolver{},
+		Directives: generated.DirectiveRoot{},
+		Complexity: generated.ComplexityRoot{},
+	}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
